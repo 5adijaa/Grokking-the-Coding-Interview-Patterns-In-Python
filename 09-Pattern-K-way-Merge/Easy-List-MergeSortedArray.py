16e035985a78656c2b9a2c218a5c2ff070db62c6
5adijaa <tal7akhadija@gmail.com>
2023-04-09 12:51:23 +0000

    Merge Sorted Array TC:O(m+n) and SC:O(1)

--- 09-Pattern-K-way-Merge/Easy-List-MergeSortedArray.py
+++ 09-Pattern-K-way-Merge/Easy-List-MergeSortedArray.py
@@ -31,23 +31,59 @@ def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     '''
     Do not return anything, modify nums1 in-place instead.
     '''
-    min_heap = []
+    # Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
 
-    for p1 in range(m):
-        heappush(min_heap, nums1[p1])
-    
-    for p2 in range(n):
-        heappush(min_heap, nums2[p2])
-    
-    i = 0
-    while min_heap and i < n+m:
-        nums1[i] = heappop(min_heap)
-        i+=1
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
     
     return nums1
 
+def main():
+    m = [3, 1, 0, 9, 2, 3, 1, 8]
+    n = [3, 0, 1, 6, 1, 4, 2, 1]
+    nums1 = [
+        [1, 2, 3, 0, 0, 0],
+        [1],
+        [0],
+        [23, 33, 35, 41, 44, 47, 56, 91, 105, 0, 0, 0, 0, 0, 0], 
+        [1, 2, 0], 
+        [1, 1, 1, 0, 0, 0, 0], 
+        [6, 0, 0], 
+        [12, 34, 45, 56, 67, 78, 89, 99, 0]
+    ]
+    nums2 = [
+        [2, 5, 6],
+        [],
+        [1],
+        [32, 49, 50, 51, 61, 99], 
+        [7], 
+        [1, 2, 3, 4], 
+        [-99, -45], 
+        [100]
+    ]
+    k = 1
+    for i in range(len(m)):
+        print(k, '.\tnums1: ', nums1[i], ', m: ', m[i], sep = '')
+        print('\tnums2: ', nums2[i], ', n: ', n[i], sep = '')
+        print('\n\tMerged list: ', merge(nums1[i], m[i], nums2[i], n[i]), sep = '')
+        print('-'*100, '\n')
+        k += 1
+
+
+if __name__ == '__main__':
+    main()
+
 '''
-TC -> O((m+n)log(m+n)), where m and n are the lengths of the input arrays nums1 and nums2, respectively. This is because our algorithm first pushes all elements from nums1 and nums2 into a min heap, which takes O(m+n) time. Then, it pops elements from the heap and assigns them to nums1, which takes O((m+n)log(m+n)) time.
-SC -> O(m+n) because we created a min heap of size m+n to store all the elements from nums1 and nums2
+TC -> O(m+n), where m and n are the lengths of the input arrays nums1 and nums2, respectively.
+SC -> O(1)
 '''
 
